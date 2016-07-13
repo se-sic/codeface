dc7100b77d2eee0efc0944c7ae6253e8e68eeab6
Claus Hunsen <hunsen@fim.uni-passau.de>
2016-07-13 12:41:14 +0200

    Remove branch 'schubert' from Travis CI
    As the changes on the branch 'schubert' influence the test suite on
    purpose, the branch should be excluded from Travis CI as the test suite
    will always fail.
    
    IMPORTANT: This is NOT wanted in upstream Codeface.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- packages.r
+++ packages.r
@@ -49,7 +49,7 @@ reinstall.package.from.github <- function(package, url) {
     }
 
     ## Re-install packages
-    devtools::install_github(url, quiet=T)
+    devtools::install_github(url, quiet=F)
 }
 
 library(parallel)
@@ -80,7 +80,8 @@ p <- filter.installed.packages(c("statnet", "tm", "optparse", "arules", "data.ta
                                  "RCurl", "mgcv", "shiny", "dtw", "httpuv", "devtools",
                                  "corrgram", "logging", "png", "rjson", "lsa", "RJSONIO"))
 if(length(p) > 0) {
-    install.packages(p, dependencies=T, verbose=F, quiet=T, Ncpus=num.cores)
+    install.packages(p, dependencies=T, verbose=T, quiet=F, Ncpus=num.cores,
+    repos="http://cran.rstudio.com/")
 }
 
 ## Install following packages from different sources
