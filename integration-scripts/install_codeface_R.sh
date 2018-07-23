51b34aaf5f6fccefd549da78ecc0c62d468ecf17
Claus Hunsen <hunsen@fim.uni-passau.de>
2018-07-23 17:39:45 +0200

    Add libgdal1-dev to dependencies
    As some R-package dependencies in turn depend on the gdal-config binary,
    we need to add libgdal1-dev – which provides this very binary – as a
    system dependency for TravisCI or any system where Codeface is to be
    installed.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- integration-scripts/install_codeface_R.sh
+++ integration-scripts/install_codeface_R.sh
@@ -13,7 +13,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install \
 	r-cran-xtable r-cran-reshape r-cran-stringr r-cran-scales \
 	r-cran-scales r-cran-rmysql r-cran-rcurl r-cran-mgcv \
 	r-cran-rjson r-cran-testthat libx11-dev libssl-dev libssh2-1-dev \
-	libudunits2-dev
+	libudunits2-dev libgdal1-dev
 
 echo "Providing R libraries - packages.r"
 
