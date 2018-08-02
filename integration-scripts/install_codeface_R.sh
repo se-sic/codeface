6be95443eaf9086197069c9e1642ce3fc159ef3f
Claus Hunsen <hunsen@fim.uni-passau.de>
2018-08-02 10:43:08 +0200

    Update libgdal-dev to version 2.0+
    As an update to commit 51b34aaf5f6fccefd549da78ecc0c62d468ecf17, we
    update the libgdal-dev package to version 2.0+ to really ensure
    compatibility.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- integration-scripts/install_codeface_R.sh
+++ integration-scripts/install_codeface_R.sh
@@ -13,7 +13,12 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install \
 	r-cran-xtable r-cran-reshape r-cran-stringr r-cran-scales \
 	r-cran-scales r-cran-rmysql r-cran-rcurl r-cran-mgcv \
 	r-cran-rjson r-cran-testthat libx11-dev libssl-dev libssh2-1-dev \
-	libudunits2-dev libgdal1-dev
+	libudunits2-dev
+
+# install newer version of GDAL for compatibility with automatically selected packages
+sudo add-apt-repository -y ppa:ubuntugis/ppa
+sudo apt-get update -qq
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install libgdal-dev libgdal20
 
 echo "Providing R libraries - packages.r"
 
